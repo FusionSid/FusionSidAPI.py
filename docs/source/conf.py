42156bb618e251296d641546389347d929e7dc41
FusionSid <siddheshadsv@icloud.com>
2022-03-18 15:49:29 +1300

    Fixed docs?

---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+from fusionsid import *
+
+project = 'FusionSidAPI.py'
+copyright = '2022, Siddhesh Zantye'
+author = 'Siddhesh Zantye'
+
+release = '0.0.7'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'renku'
+
+html_static_path = ['_static']
